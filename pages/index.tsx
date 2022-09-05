90b7a113f88da0612713cccf1a2860592c635efa
slopezdavinci <sebastian.lopez@davinci.edu.ar>
2022-09-05 14:11:39 -0300

    using products by hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,13 @@
-import {
-  Card,
-  CardActionArea,
-  CardMedia,
-  Grid,
-  Typography,
-} from "@mui/material";
+import { Typography } from "@mui/material";
 import type { NextPage } from "next";
 import { ShopLayout } from "../components/layouts";
 import { ProductList } from "../components/products";
-import { initialData } from "../database/products";
+import { FullScreenLoading } from "../components/ui";
+import { useProducts } from "../hooks";
 
-const Home: NextPage = () => {
+const HomePage: NextPage = () => {
+  const { products, isLoading } = useProducts("/products");
+
   return (
     <ShopLayout
       title={"Teslo Shop"}
@@ -27,12 +24,9 @@ const Home: NextPage = () => {
       >
         Todos los productos
       </Typography>
-
-      
-        <ProductList products={initialData.products as any} />
-      
+      {isLoading ? <FullScreenLoading /> : <ProductList products={products} />}
     </ShopLayout>
   );
 };
 
-export default Home;
+export default HomePage;
