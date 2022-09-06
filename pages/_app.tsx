03f0089d90b71adca302e4e164211ee129c9b103
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2022-09-06 12:09:41 -0300

    context uppercase fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { lightTheme } from "../themes";
 import { SWRConfig } from "swr";
-import { UIProvider } from "../context";
+import { UiProvider } from "../context";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -13,12 +13,12 @@ function MyApp({ Component, pageProps }: AppProps) {
           fetch(resource, init).then((res) => res.json()),
       }}
     >
-      <UIProvider>
+      <UiProvider>
         <ThemeProvider theme={lightTheme}>
           <CssBaseline />
           <Component {...pageProps} />
         </ThemeProvider>
-      </UIProvider>
+      </UiProvider>
     </SWRConfig>
   );
 }
