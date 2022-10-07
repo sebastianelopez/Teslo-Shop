01e59c9723b2987c32d28b7d7ac2b3d742722a76
sebastianelopez <lopezsebastian.emanuel@gmail.com>
2022-10-07 17:55:08 -0300

    form validations applied

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,52 +1,87 @@
 import { Box, Button, Grid, TextField, Typography, Link } from "@mui/material";
 import { AuthLayout } from "../../components/layouts";
 import NextLink from "next/link";
+import { useForm } from "react-hook-form";
+import { validations } from "../../utils";
+
+type FormData = {
+  email: string;
+  password: string;
+};
 
 const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onLoginUser = (data: FormData) => {};
+
   return (
     <AuthLayout title={"Login"}>
-      <Box
-        sx={{
-          width: 350,
-          padding: "10px 20px",
-        }}
-      >
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Typography variant="h1" component={"h1"}>
-              Ingresar
-            </Typography>
-          </Grid>
+      <form onSubmit={handleSubmit(onLoginUser)} noValidate>
+        <Box
+          sx={{
+            width: 350,
+            padding: "10px 20px",
+          }}
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant="h1" component={"h1"}>
+                Ingresar
+              </Typography>
+            </Grid>
 
-          <Grid item xs={12}>
-            <TextField label="Correo" variant="filled" fullWidth />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              label="Contraseña"
-              type={"password"}
-              variant="filled"
-              fullWidth
-            />
-          </Grid>
+            <Grid item xs={12}>
+              <TextField
+                type={"email"}
+                label="Correo"
+                variant="filled"
+                fullWidth
+                {...register("email", {
+                  required: "Este campo es requerido",
+                  validate: validations.isEmail,
+                })}
+                error={!!errors.email}
+                helperText={errors.email?.message}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Contraseña"
+                type={"password"}
+                variant="filled"
+                fullWidth
+                {...register("password", {
+                  required: "Este campo es requerido",
+                  minLength: { value: 6, message: "Minimo 6 caracteres" },
+                })}
+                error={!!errors.password}
+                helperText={errors.password?.message}
+              />
+            </Grid>
 
-          <Grid item xs={12}>
-            <Button
-              color="secondary"
-              className="circular-btn"
-              size="large"
-              fullWidth
-            >
-              Ingresar
-            </Button>
+            <Grid item xs={12}>
+              <Button
+                type="submit"
+                color="secondary"
+                className="circular-btn"
+                size="large"
+                fullWidth
+              >
+                Ingresar
+              </Button>
+            </Grid>
+            <Grid item xs={12}>
+              <NextLink href={"/auth/register"} passHref>
+                <Link underline="always">Registrarse</Link>
+              </NextLink>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <NextLink href={"/auth/register"} passHref>
-              <Link underline="always">Registrarse</Link>
-            </NextLink>
-          </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </form>
     </AuthLayout>
   );
 };
